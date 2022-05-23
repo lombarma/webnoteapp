51b3ffc0ed1b6b3fb785e280fecf5f81bc4fa661
lombarma <maxime.lombardo2@edu.esiee.fr>
2022-05-23 18:41:11 +0200

    a

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)  # every change in the code will automatically rerun the code
+    app.run(debug=False, host='0.0.0.0')  # every change in the code will automatically rerun the code
